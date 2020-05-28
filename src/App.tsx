5f1459ed04458defb0af097230fad5da8b05caeb
keiya01 <keiya.s.0210@gmail.com>
2020-05-29 00:56:12 +0900

    Added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useCallback, ReactElement, useRef } from 'react';
+import { useForm, Control } from 'react-hook-form';
 import './App.css';
 
+type FormValue = Record<string, string>;
+
+interface InputListProps {
+  inputCount: number;
+  register: Control['register'];
+}
+const InputList = ({inputCount, register}: InputListProps): ReactElement => (
+  <>
+    {[...new Array(inputCount)].map((_, i) => <input key={i} name={`${i}`} ref={register} className="input" />)}
+  </>
+);
+
+interface ResultProps {
+  values: FormValue | null;
+}
+const Result = ({ values }: ResultProps): ReactElement | null => 
+    values
+    ?
+    <div className="result-container">
+      <h1 className="title">Your input values</h1>
+      <ul>
+        {Object.keys(values).map((key, i) => (<li key={i}>{values[key]}</li>))}
+      </ul>
+    </div>
+  : null;
+
 function App() {
+  const { register, handleSubmit } = useForm<FormValue>();
+  const [inputCount, setInputCount] = useState(1);
+  const values = useRef<FormValue|null>(null);
+
+  const handleOnClick = useCallback(() => {
+    setInputCount(prev => prev + 1);
+  }, []);
+
+  const handleOnSubmit = useCallback((data: Record<string, any>) => {
+    values.current = data;
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="form">
+        <h1 className="title">Enter something</h1>
+        <InputList inputCount={inputCount} register={register}/>
+        <button type="button" className="plus-button" onClick={handleOnClick} aria-label="add form">+</button>
+        <button type="button" className="submit-button" onClick={handleSubmit(handleOnSubmit)}>submit</button>
+      </form>
+      <Result values={values.current} />
     </div>
   );
 }
